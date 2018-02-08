99e15324a260407081c44901eab1483ab4b4048f
Kristina Senesaynho <ksenesaynho@gmail.com>
2018-02-08 16:33:56 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,6 +2,14 @@ class CashRegister
   
   attr_accessor :total, :items, :discount, :last_transaction
   
+  def initialize(total, items, discount, last_transaction)
+    @total
+    @items
+    @discount
+    @last_transaction
+  
+  
+  
 end
   
   
